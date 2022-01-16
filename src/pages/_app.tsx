48efb334ecaf9b75dbcf9ce3d173a315ea870327
Vlad Duma <vlad.duma@deplabs.com>
2022-01-16 16:53:58 +0200

    update : add next commerce

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -6,12 +6,40 @@ import 'scss/main.scss';
 import { client } from 'client';
 import type { AppProps } from 'next/app';
 
+// export default function MyApp({ Component, pageProps }: AppProps) {
+//   return (
+//     <>
+//       <FaustProvider client={client} pageProps={pageProps}>
+//         <Component {...pageProps} />
+//       </FaustProvider>
+//     </>
+//   );
+// }
+// import '@assets/main.css'
+// import '@assets/chrome-bug.css'
+// import 'keen-slider/keen-slider.min.css'
+
+import { FC, useEffect } from 'react'
+import { ManagedUIContext } from '../components/ui/context'
+
+const Noop: FC = ({ children }) => <>{children}</>
+
 export default function MyApp({ Component, pageProps }: AppProps) {
+  const Layout = (Component as any).Layout || Noop
+
+  useEffect(() => {
+    document.body.classList?.remove('loading')
+  }, [])
+
   return (
     <>
       <FaustProvider client={client} pageProps={pageProps}>
-        <Component {...pageProps} />
+        <ManagedUIContext>
+          <Layout pageProps={pageProps}>
+            <Component {...pageProps} />
+          </Layout>
+        </ManagedUIContext>
       </FaustProvider>
     </>
-  );
+  )
 }
